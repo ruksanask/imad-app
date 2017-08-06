5cb77b8d4726b079d45bc361e4af12e06132207b
ruksanask <30727765+ruksanask@users.noreply.github.com>
2017-08-06 07:03:05 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
-console.log('Loaded!');
+//counter
+var button=document.getElementById('counter');
+button.onlick=function()
+{
+  //make a request to the counter end points
+  
+  //capture the store point
+  
+  //render the counter variable span
+  counter=counter+1;
+  var span=document.getElementById('count');
+  span.innerHTML=counter.toString();
+  
+  
+};
