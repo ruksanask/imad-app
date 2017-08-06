c2ce3ce95ede242d56d3de638178b24d3c92df16
ruksanask <30727765+ruksanask@users.noreply.github.com>
2017-08-06 09:46:06 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,14 +4,24 @@ var counter=0;
 button.onclick=function()
 
 {
+  var request=XMLHttprequest();
   
   
   
   
   
+  request.onreadystatechange=function(){
+      if(request.readystate===XMLHttprequest.Done){
+          if(request.status===200){
+            var counter=  request.responseText;
+           var span=document.getElementById('count');
+           span.innerHTML=counter.toString();
+                   }
+      }
+  };
   
-  counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerHTML=counter.toString();
+  request.open('GET', "http://sanaskruksana69.imad.hasura-app.io/counter", true);
+  request.send(null);
+  
   
 };
