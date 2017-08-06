c9a819eb3020a0e3aa3b4a9545fb671210cd3c2e
ruksanask <30727765+ruksanask@users.noreply.github.com>
2017-08-06 09:38:32 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,17 @@
-///counter
+//counter
 var button=document.getElementById('counter');
-
+var counter=0;
 button.onclick=function()
+
 {
-    // create a request object
-    
-    var request= new XMLHttpRequest();
-  //make a request to the counter end points
   
-  //capture the store point
-  request.onreadystatechange = function ()
-  {
-      if(request.readystate === XMLHttpRequest.DONE)
-      {
-    // take action
-    
-    if(request.status===200){
-        var counter=request.responseText;
-         var span=document.getElementById('count');
-         span.innerHTML=counter.toString();
-    }
-      }
-      //not done yet
-}; 
- // make a request
- request.open('GET','http://http://sanaskruksana69.imad.hasura-app.io/counter',true);
- request.send(null);
+  
+  
+  
+  
+  
+  counter=counter+1;
+  var span=document.getElementById('count');
+  span.innerHTML=counter.toString();
+  
 };
